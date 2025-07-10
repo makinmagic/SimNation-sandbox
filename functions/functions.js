785cb09ff2ebb2a615c57c791308e772bc9decfc
makinmagic <timbityyc@gmail.com>
2025-07-10 09:44:23 -0600

    Update functions.js

--- functions/functions.js
+++ functions/functions.js
@@ -523,8 +523,8 @@ function setClockLabel() {
     let tsomins = Math.floor((cycle % 300) / 5);
     if (tsomins < 10) tsomins = "0" + tsomins;
 
-    const formattedTime = `${tsohours}⸱${tsomins} ${suffix}`;
-    document.getElementById("tempoSim").textContent = formattedTime;
+    const formattedTime = `${tsohours}<wbr>:<wbr>${tsomins} <wbr>${suffix}`;
+    document.getElementById("tempoSim").innerHTML = formattedTime;
 
     setTimeout(tempoSim, 1000);
 }
